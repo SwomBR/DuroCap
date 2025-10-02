4b2a31274cfc4d6eb5024703f39e43acbf55c72f
AnjanaUnnikrishnan24 <anjana24unni@gmail.com>
2025-10-02 22:24:01 +0530

    update

--- Server/Models/User.js
+++ Server/Models/User.js
@@ -1,19 +1,36 @@
 import { Schema, model } from "mongoose";
 
-const userSchema = new Schema({
-  name: { type: String, required: true, trim: true, minlength: 2 },
-  email: { type: String, required: true, unique: true, lowercase: true, trim: true, match: [/^\S+@\S+\.\S+$/, "Invalid email format"] },
-  phone: {  type: String,  required: true,  trim: true,  match: [/^[0-9]{7,15}$/, "Invalid phone number"] },
+const addressSchema = new Schema({
   address: { type: String, required: true, trim: true },
   district: { type: String, required: true, trim: true },
   state: { type: String, required: true, trim: true },
   country: { type: String, required: true, trim: true },
-  pinCode: { type: Number, required: true, min: 100000, max: 999999 },
+  pinCode: { type: Number, required: true, min: 100000, max: 999999 }
+}, { _id: false }); // 👈 prevents extra _id for each address
+
+const userSchema = new Schema({
+  name: { type: String, required: true, trim: true, minlength: 2 },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true, 
+    lowercase: true, 
+    trim: true, 
+    match: [/^\S+@\S+\.\S+$/, "Invalid email format"] 
+  },
+  phone: {  
+    type: String,  
+    required: true,  
+    trim: true,  
+    match: [/^[0-9]{7,15}$/, "Invalid phone number"] 
+  },
   password:{ type:String, required: true },
   userRole:{ type:String, enum:["admin", "user"], default: "user" },
+  addresses: [addressSchema],
   createdAt: { type: Date, default: Date.now }
 }, { timestamps: true });
 
 const User = model("User", userSchema);
 
 export default User;
+
