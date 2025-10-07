83881b70c12b686c3c93502af41c6e891930bcba
AnjanaUnnikrishnan24 <anjana24unni@gmail.com>
2025-10-07 11:43:17 +0530

    update

--- Frontend/src/pages/SplashScreen.jsx
+++ Frontend/src/pages/SplashScreen.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './SplashScreen.css';
 
 const SplashScreen = () => {
   const navigate = useNavigate();
