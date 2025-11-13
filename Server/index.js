0d3cde329ed7a1fdcb94fd3f6915aab42d6081b4
Advaithdp <advaithdp@email.com>
2025-11-13 20:57:32 +0530

    added cors linkage

--- Server/index.js
+++ Server/index.js
@@ -16,10 +16,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-    origin:'http://localhost:3000',
-    credentials:true
-}))
+app.use(cors());
 
 app.use(json());
 app.use(cookieParser()); 
