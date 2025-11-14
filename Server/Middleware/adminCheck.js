198de5f83331dae6949206bef30a755f01cf8439
AnjanaUnnikrishnan24 <anjana24unni@gmail.com>
2025-11-14 20:09:14 +0530

    update

--- Server/Middleware/adminCheck.js
+++
@@ -1,8 +0,0 @@
-const adminCheck = (req, res, next) => {
-    if (!req.user || req.user.userRole !== "admin") {
-        return res.status(403).json({ message: "Only Admins can access." });
-    }
-    next();
-};
-
-export default adminCheck
--- Server/Middleware/userCheck.js
+++
@@ -1,8 +0,0 @@
-const userCheck = (req, res, next) => {
-    if (!req.user || req.user.userRole !== "user") {
-        return res.status(403).json({ message: "Only users can access." });
-    }
-    next();
-};
-
-export default userCheck;
